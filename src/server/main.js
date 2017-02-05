d77fa6022e8aa7ea38b71b6fcf8447176b579986
Bryan Fillmer <bryan.fillmer@ca.com>
2017-02-05 12:08:33 -0700

    Simplified the server, added docker.

--- src/server/main.js
+++ src/server/main.js
@@ -9,23 +9,16 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import swapiSchema from '../schema';
+import cors from 'cors';
 
 const app = express();
 
-// Requests to /graphql redirect to /
-app.all('/graphql', (req, res) => res.redirect('/'));
+app.use(cors());
 
 app.use('/', graphqlHTTP(() => ({
-  schema: swapiSchema,
-  graphiql: true
+  schema: swapiSchema
 })));
 
-// Listen for incoming HTTP requests
-const listener = app.listen(() => {
-  let host = listener.address().address;
-  if (host === '::') {
-    host = 'localhost';
-  }
-  const port = listener.address().port;
-  console.log('Listening at http://%s%s', host, port === 80 ? '' : ':' + port);
-});
+app.listen(9000, () => console.log(
+  'GraphQL Server is now running on http://localhost:9000'
+));
